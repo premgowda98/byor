4d276e2c04912413d09074437a09e68168608d08
Prem Kumar G <premkumarmvks@gmail.com>
2024-11-11 22:37:42 +0530

    multiple concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,4 +1,21 @@
 import socket  # noqa: F401
+import threading
+import time
+
+def concurrent_request(conn_object, addr):
+    while True:
+        print("\nRecieved Request from addr", addr)
+        data = conn_object.recv(2046)
+        if not data:
+            print("Client Disconnected")
+            break
+
+        print("Client data recieved" ,data)
+        time.sleep(5)
+        conn_object.sendall(b"+PONG\r\n")
+        print("Request Processed\n")
+
+    conn_object.close()
 
 
 def main():
@@ -8,21 +25,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection_object, addr = server_socket.accept() # wait for client
 
     while True:
+        connection_object, addr = server_socket.accept() # wait for client
         print("Client", addr, "has connected")
         print("Waiting for request")
-        data = connection_object.recv(2046)
-        if not data:
-            print("Client Disconnected")
-            break
-
-        print("Client data" ,data)
-        connection_object.sendall(b"+PONG\r\n")
-        print("Request Processed")
-
-    connection_object.close()
+        threading.Thread(target=concurrent_request, args=[connection_object, addr]).start()
 
 
 if __name__ == "__main__":
